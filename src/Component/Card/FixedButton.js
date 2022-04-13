b4e8b994c595184b8b98df941c73cf45d42662b2
thejey-ui <88413455+thejey-ui@users.noreply.github.com>
2022-04-13 13:44:34 +0900

    [lesh] 리렌더링 이슈 - memo 의 status 변경 시

--- src/Component/Card/FixedButton.js
+++ src/Component/Card/FixedButton.js
@@ -19,9 +19,17 @@ const FixedButton = ({ isFixed, onToggleFixed }) => {
 			onClick={onToggleFixed}
 		>
 			<Icon />
-			<ScreenOnlyText>{isFixed ? "고정 해제" : "고정"}</ScreenOnlyText>
+			<div>{isFixed ? "고정 해제" : "고정"}</div>
 		</button>
 	);
 };
 
 export default FixedButton;
+
+// issue
+/*
+	isFixed 값이 수정될 때 memocard component 하위의 모든 컴포넌트를 새로 그림.
+	이후 원본 사이트처럼 position 값만 바꾸도록 작업 진행할 예정. 
+	이 좌표 값을 store에 저장해두려고 했으나,
+	리렌더링이 일어나므로, 적합한 지 의문.
+*/
