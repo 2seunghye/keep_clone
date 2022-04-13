13f64db7e435c64b7923a8f07ce5d6bfc6be3dbd
thejey-ui <88413455+thejey-ui@users.noreply.github.com>
2022-04-13 13:50:19 +0900

    [lesh] memo content slice 수정 - single content crud 추가

--- src/module/memoContents/index.js
+++ src/module/memoContents/index.js
@@ -10,18 +10,39 @@ export const contentsSlice = createSlice({
 		removeContents: removeData.byObjectType,
 		updateSingleContent : (_prev, _action)=>{
 			const {memoId : id, type} = _action;
+			// find single content
 			const target_content = _prev[id];
-			const new_content = updateData.byArrayType(target_content, {..._action, id});
+			// update single content
+			const new_content = updateData.byArrayType(target_content, _action);
+			// re-pack
 			const repackage_action = {
-				id,
 				type, 
+				id,
 				payload : new_content
 			};
+			// push to store
+			return updateData.byObjectType(_prev, repackage_action);
+		},
+		removeSingleContent : (_prev, _action)=>{
+			console.log(_action);
+			const {memoId : id, type} = _action;
+			// find single content
+			const target_content = _prev[id];
+			console.log(current(_prev)[id]);
+			// update single content
+			const new_content = removeData.byArrayType(target_content, _action);
+			// re-pack
+			const repackage_action = {
+				type, 
+				id,
+				payload : new_content
+			};
+			// push to store
 			return updateData.byObjectType(_prev, repackage_action);
 		}
 	},
 });
-export const {createContents, updateContents, removeContents, updateSingleContent} = contentsSlice.actions;
+export const {createContents, updateContents, removeContents, updateSingleContent, removeSingleContent} = contentsSlice.actions;
 export const selectContents = (state) => state.memoContents;
 export const selectContentsById = (keyName) => (state) => (state.memoContents[keyName]);
 export default contentsSlice.reducer;
