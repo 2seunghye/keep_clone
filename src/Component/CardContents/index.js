c1b7ad03c9416d342c76bd5e1dc2ea23a58ab049
thejey-ui <88413455+thejey-ui@users.noreply.github.com>
2022-04-13 14:57:15 +0900

    [lesh] 컴포넌트 분리 - checkboxitem

--- src/Component/CardContents/index.js
+++ src/Component/CardContents/index.js
@@ -1,102 +1,9 @@
 import React from "react";
-import Styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import { removeContents, selectContentsById, updateSingleContent, removeSingleContent } from "../../module/memoContents";
-const Icon = Styled.i`
-	display:inline-block;
-	vertical-align:middle;
-	background-color:white;
-	width:18px;
-	height:18px;
-	padding:2px;
-	box-sizing:content-box;
-`;
-const Item = Styled.div`
-	display:flex;
-	padding:10px;
-	&:not(:first-child) {
-		margin-top:15px;
-	}
-	[contenteditable] {
-		flex:1;
-		padding:0 5px;
-		white-space:nowrap;
-		text-overflow:ellipsis;
-		overflow:hidden;
-	}
-`;
+import { selectContentsById, updateSingleContent, removeSingleContent } from "../../module/memoContents";
+import CheckboxItem from "./CheckboxItem";
 
-function CheckboxTypeItem({memoId, content}){
-	console.log("rendering check : content item checkbox");
-	const {id, text, isChecked} = content;
-	const dispatch = useDispatch();
-	const removeContent = ()=>{
-		const payload = {
-			...content
-		};
-		const action = Object.assign(
-			removeSingleContent(payload), 
-			{memoId}
-		);
-		dispatch(action);
-	};
-	const updateContentText = (event) => {
-		// escape:equal after and before; 
-		if(text === event.target.innerText) return false;
-		// escape:there is no letters
-		if(0 >= event.target.innerText.length) return removeContent();
-		const payload = {
-			...content,
-			text : event.target.innerText,
-		};
-		const action = Object.assign(
-			updateSingleContent(payload), 
-			{memoId}
-		);
-		dispatch(action);
-	};
-	const toggleChecked = () => {
-		const payload = {
-			...content,
-			isChecked : !isChecked
-		};
-		const action = Object.assign(
-			updateSingleContent(payload), 
-			{memoId}
-		);
-		dispatch(action);
-	};
-	const keydown = (event)=>{
-		if(event.key !== "Enter") return false;
-		console.log("enter key!");
-		updateContentText(event);
-	};
-	return(
-		<Item>
-			<label htmlFor={id}>
-				<input 
-					id={id}
-					type="checkbox" 
-					checked={isChecked}
-					onChange={toggleChecked}
-				/>
-			</label>
-			<div 
-				contentEditable="true" 
-				spellCheck="true"
-				onBlur={updateContentText}
-				onKeyDown={keydown}
-			>
-				{text}		
-			</div>
-			<button type="button" onClick={removeContent}>
-				<Icon />
-				<span>{"Delete"}</span>
-			</button>
-		</Item>
-	);
-}
-function DefaultTypeItem({contents}){
+function TextEditor({memoId, contents}){
 	console.log("rendering check : content item default");
 	const to_text = contents.map(content => content.text).join("\n\n");
 	const parse_text = to_text.replaceAll(/(\n)\s/g, "<br/>")
@@ -118,7 +25,7 @@ function IndicatorLatestTime({date}){
 }
 function CardContents({className, memoId, useCheckbox}){
 	console.log("rendering check : card contents");
-	const contents = useSelector(selectContentsById(memoId));
+	const contents = useSelector(selectContentsById(memoId)) || [];
 	return(
 		<div className={className}>
 			{0 >= contents.length && 
@@ -126,15 +33,20 @@ function CardContents({className, memoId, useCheckbox}){
 					placeholderText={"메모 작성..."} />
 			}
 			{
-				useCheckbox ?
+				useCheckbox 
+				?
+				// checkbox list
 				contents.map((content) => (
-					<CheckboxTypeItem 
+					<CheckboxItem
 						key={content.id}
 						memoId={memoId}
 						content={content}
 					/>
-				)) :
-				<DefaultTypeItem 
+				)) 
+				:
+				// text editor
+				<TextEditor 
+					memoId={memoId}
 					contents={contents}/>
 			}
 			<IndicatorLatestTime 
