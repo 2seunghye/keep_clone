af838fcdc20b926476d4fcbc28b6b767e83d5887
thejey-ui <88413455+thejey-ui@users.noreply.github.com>
2022-04-14 14:05:10 +0900

    [lesh] 롤백 부분 정정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 // redux
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./Redux/rootReducer";
+import rootReducer from "./redux/rootReducer";
 
 // router
 import { BrowserRouter, Route, Routes } from "react-router-dom";
