3aa8f0a2eb53dc42d95db9a2d85e89b02f2885c8
thejey-ui <88413455+thejey-ui@users.noreply.github.com>
2022-04-14 14:07:07 +0900

    [lesh]  card contents 수정  - classname 제거

--- src/Component/CardContents/index.js
+++ src/Component/CardContents/index.js
@@ -23,12 +23,12 @@ function Placeholder({placeholderText, style}){
 function IndicatorLatestTime({date}){
 	return 	<div>{`수정된 시간: ${date.getMonth() + 1}월 ${date.getDate()}일`}</div>
 }
-function CardContents({className, memoId, useCheckbox}){
+function CardContents({memoId, useCheckbox}){
 	console.log("rendering check : card contents");
 	const contents = useSelector(selectContentsById(memoId)) || [];
 	const style = {"display" : 0 >= contents.length ? null : "none"};
 	return(
-		<div className={className}>
+		<div>
 			<Placeholder 
 				placeholderText={"메모 작성..."} 
 				style={style} />
@@ -44,7 +44,7 @@ function CardContents({className, memoId, useCheckbox}){
 					/>
 				)) 
 				:
-				// text editor
+				// note
 				<TextEditor 
 					memoId={memoId}
 					contents={contents}/>
@@ -55,3 +55,16 @@ function CardContents({className, memoId, useCheckbox}){
 	)
 }
 export default CardContents;
+// issue
+/*
+	#keep 원본의 이슈
+	체크박스 -> 노트로 전환할 때, 
+	체크한 항목이 있으면 해당 항목을 지우고 노트로 전환하고,
+	체크한 항목들이 미체크 항목의 아래에 있던 그대로로 전환한다.
+	또한 undo의 대상이 아님.
+
+	이 ui를 수정할 예정. 
+	- 항목을 지우지 않고 전환.
+	- 전체 미체크 시의 순서로 전환.
+	- 반대로 전환 시에도 예전 상태를 기억하도록. (타입 변환에 상관없이 isChecked를 유지)
+*/
